80c7070b7e542f28f904e61588a1c1b5d9922c90
Máxim Sánchez Porta <maximsanchezporta@gmail.com>
2020-01-17 11:23:34 +0100

    .gitignore correjit despres de push amb funcio majorque i mitja correjides

---
+++ Projecte/calc.c
@@ -0,0 +1,26 @@
+int suma(int op1, int op2){
+	return (op1+op2);
+}
+int resta(int op1, int op2){
+	return (op1-op2);
+}
+int multiplica(int op1, int op2){
+	return (op1*op2);
+}
+int divideix(int op1, int op2){
+	return (op1/op2);
+} 
+
+int majorque(int op1, int op2){
+	if (op1>op2){
+		return (op1);
+	}
+	else{
+		return (op2);
+	}
+}
+
+int mitja(int op1, int op2){
+	int mitjana=((op1+op2)/2);
+	return mitjana;
+}
---
+++ Projecte/calc.h
@@ -0,0 +1,6 @@
+int suma(int op1, int op2);
+int resta(int op1, int op2);
+int multiplica(int op1, int op2);
+int divideix(int op1, int op2);
+int majorque(int op1, int op2);
+int mitja(int op1, int op2);
