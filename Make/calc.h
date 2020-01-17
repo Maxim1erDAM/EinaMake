f46187094be0d7348aa290b47a372426a787e719
Máxim Sánchez Porta <maximsanchezporta@gmail.com>
2020-01-17 11:26:13 +0100

    .gitignore correjit despres de push amb funcio majorque i mitja correjides

--- Make/calc.h
+++
@@ -1,6 +0,0 @@
-int suma(int op1, int op2);
-int resta(int op1, int op2);
-int multiplica(int op1, int op2);
-int divideix(int op1, int op2);
-int majorque(int op1, int op2);
-int mitja(int op1, int op2);
